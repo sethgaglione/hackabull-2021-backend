f4d3492ba35c020cf4327a55fca19a65d55e6fa5
Seth Gaglione <seth.gaglione@live.com>
2021-03-13 11:19:53 -0600

    Removed swagger

--- Hackabull2021Backend/Startup.cs
+++ Hackabull2021Backend/Startup.cs
@@ -28,10 +28,6 @@ namespace Hackabull2021Backend
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hackabull2021Backend", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +36,6 @@ namespace Hackabull2021Backend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hackabull2021Backend v1"));
             }
 
             app.UseHttpsRedirection();
