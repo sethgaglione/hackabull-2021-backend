b56cbf6f8271207ef4466e8e0cd03710b30226bc
Seth Gaglione <seth.gaglione@live.com>
2021-03-13 11:52:40 -0600

    Added User Schema

---
+++ Hackabull2021Backend/Models/User.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hackabull2021Backend.Models
+{
+    public class User
+    {
+        // Details about the User
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthday { get; set; }
+
+        // Password Stuff
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string HashinTechnique { get; set; }
+
+        // Used to differentiate Users
+        public int UserType { get; set; }
+
+        // User Photo
+        public string PhotoFileName { get; set; }
+    }
+}
