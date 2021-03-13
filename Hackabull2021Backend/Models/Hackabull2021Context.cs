10ef4c09ea545af6e2f23fdef9eaa340a9f0803c
Seth Gaglione <sethgaglione@users.noreply.github.com>
2021-03-13 12:37:53 -0600

    Added npgsql

---
+++ Hackabull2021Backend/Models/Hackabull2021Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Hackabull2021Backend.Models
+{
+    public class Hackabull2021Context : DbContext
+    {
+        public Hackabull2021Context(DbContextOptions<Hackabull2021Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test> Tests { get; set; }
+    }
+}
--- Hackabull2021Backend/Startup.cs
+++ Hackabull2021Backend/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Hackabull2021Backend.Models;
 
 namespace Hackabull2021Backend
 {
@@ -26,7 +28,8 @@ namespace Hackabull2021Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<Hackabull2021Context>(opt =>
+                                                        opt.UseNpgsql(Configuration.GetConnectionString("Database")));
             services.AddControllers();
         }
 
