30d9c8e664ae544f9b9cc1b274d0973dc914594a
Seth Gaglione <seth.gaglione@live.com>
2021-03-13 22:50:38 -0600

    Added Sequelize

--- src/ExpressServer.ts
+++ src/ExpressServer.ts
@@ -2,16 +2,30 @@ import { Server } from 'http';
 import { Application } from 'express';
 import * as helmet from 'helmet';
 import * as bodyParser from 'body-parser';
+import { Sequelize } from 'sequelize/types';
 
 export class ExpressServer {
   private app: Application;
   private server: Server;
   private PORT: string = process.env.PORT;
+  private sequelize: Sequelize;
 
   constructor() {
     const express = require('express');
     this.app = express();
     this.server = new Server(this.app);
+
+    // Set the sequelize string
+    this.sequelize = new Sequelize(process.env.DATABASE_URL);
+
+    // Test connection
+    this.sequelize.authenticate().then(() => {
+      console.log('Connection established successfully.');
+    }).catch(err => {
+      console.error('Unable to connect to database: ', err);
+    }).finally(() => {
+      this.sequelize.close();
+    });
 
     // Use helmet middleware
     const helmet = require('helmet');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,18 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize: Sequelize = new Sequelize('');
-
-export const User = sequelize.define('user', {
-  id: DataTypes.INTEGER,
-  firstName: DataTypes.STRING,
-  lastName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  userName: DataTypes.STRING,
-  phoneNumber : DataTypes.STRING,
-  birthday: DataTypes.STRING,
-  password: DataTypes.STRING,
-  salt: DataTypes.STRING,
-  hashinTechnique: DataTypes.STRING,
-  userType: DataTypes.INTEGER,
-  photoFileName: DataTypes.STRING,
-});
+export const User = ( sequelize: Sequelize ) => {
+  return sequelize.define('user', {
+    id: DataTypes.INTEGER,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    phoneNumber : DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    password: DataTypes.STRING,
+    salt: DataTypes.STRING,
+    hashinTechnique: DataTypes.STRING,
+    userType: DataTypes.INTEGER,
+    photoFileName: DataTypes.STRING,
+  });
+}
